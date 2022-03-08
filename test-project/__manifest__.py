92ff84e49beb049023b2a2d0f40ac5f211363628
Jona <j.cara@commprog.com>
2022-03-08 14:34:41 +0000

    [ADD] Added init and manifest

---
+++ test-project/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+'name': 'Jona',
+'version': '1.0',
+'category': 'Practice',
+'description': """
+Test Module
+===========================================================================
+""",
+'author': 'Jona',
+'website': '',
+'depends': ['base'],
+
+# always loaded
+'data': [],
+# only loaded in demonstration mode
+'demo': [
+],
+'css': [],
+'installable': True,
+'application': True,
+'auto_install': False,
+'qweb': [],
+}
